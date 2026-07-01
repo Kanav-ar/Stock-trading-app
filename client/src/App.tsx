daf41ec20b15aca7d0db12defa5de080ea7c8966
Kanav-ar <kanava879@gmail.com>
2026-07-01 18:18:50 +0530

    add initial components and pages for the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 // import { useState } from 'react'
 
+import Playground from "./pages/playground"
+
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Playground/>
     </>
   )
 }
---
+++ client/src/components/layout/Navbar.tsx
@@ -0,0 +1,22 @@
+
+import logo from '../assets/logo.svg'
+export default function Navbar() {
+  return (
+    <>
+        <div>
+            <div className='flex justify-between items-center px-55 py-5'>
+            <div>
+                <img src={logo} alt="logo" className='w-32.25'/>
+            </div>
+            <div id='nav-tags' className='w-[50%] flex justify-around px-4'>
+                <a href='' className='opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor'>Signup</a>
+                <a href='' className='opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor'>About</a>
+                <a href='' className='opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor'>Products</a>
+                <a href='' className='opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor'>Pricing</a>
+                <a href='' className='opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor'>Support</a>
+            </div>
+            </div>
+        </div>
+    </>
+  );
+}
