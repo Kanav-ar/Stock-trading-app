ca74cb427d3ff7bf4d7fc1f0b4338d92e704a1ed
Kanav-ar <kanava879@gmail.com>
2026-07-03 13:13:39 +0530

    create new component files for home, pages, and layout

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,22 +1,50 @@
-
-import logo from '../assets/logo.svg'
+import logo from "../../assets/logo.svg";
 export default function Navbar() {
   return (
     <>
-        <div>
-            <div className='flex justify-between items-center px-55 py-5'>
-            <div>
-                <img src={logo} alt="logo" className='w-32.25'/>
-            </div>
-            <div id='nav-tags' className='w-[50%] flex justify-around px-4'>
-                <a href='' className='opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor'>Signup</a>
-                <a href='' className='opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor'>About</a>
-                <a href='' className='opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor'>Products</a>
-                <a href='' className='opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor'>Pricing</a>
-                <a href='' className='opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor'>Support</a>
-            </div>
-            </div>
+      <div>
+        <div className="flex justify-between items-center px-10 py-5">
+          <div className="">
+            <img src={logo} alt="logo" className="w-32.25" />
+          </div>
+          <div id="nav-tags" className=" w-[50%] flex justify-around px-4">
+            <a
+              href=""
+              className="opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor"
+            >
+              Signup
+            </a>
+            <a
+              href=""
+              className="opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor"
+            >
+              About
+            </a>
+            <a
+              href=""
+              className="opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor"
+            >
+              Products
+            </a>
+            <a
+              href=""
+              className="opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor"
+            >
+              Pricing
+            </a>
+            <a
+              href=""
+              className="opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor"
+            >
+              Support
+            </a>
+          </div>
+          <div>
+            <a href=""></a>
+                
+          </div>
         </div>
+      </div>
     </>
   );
 }
