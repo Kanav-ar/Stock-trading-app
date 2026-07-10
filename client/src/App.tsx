720931617990421d7a1148a48a80ac20d8b6d471
Kanav-ar <kanava879@gmail.com>
2026-07-11 02:03:59 +0530

    complete navbar layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
-
+import { useState } from "react"
 import Playground from "./pages/playground"
 
 function App() {
-
+  const [theme, setTheme] = useState("light")
   return (
     <>
       <Playground/> 
--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,48 +1,53 @@
+import { useState } from "react";
 import logo from "../../assets/logo.svg";
+import { Menu,Moon,Sun } from "lucide-react";
+
+
 export default function Navbar() {
+  const [isDark,setIsDark] = useState(true);
   return (
     <>
-      <div>
-        <div className="flex justify-between items-center px-10 py-5">
-          <div className="">
-            <img src={logo} alt="logo" className="w-32.25" />
-          </div>
-          <div id="nav-tags" className=" w-[50%] flex justify-around px-4">
-            <a
-              href=""
-              className="opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor"
-            >
-              Signup
-            </a>
-            <a
-              href=""
-              className="opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor"
-            >
-              About
-            </a>
-            <a
-              href=""
-              className="opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor"
-            >
-              Products
-            </a>
-            <a
-              href=""
-              className="opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor"
-            >
-              Pricing
-            </a>
-            <a
-              href=""
-              className="opacity-75 hover:text-blue-500 hover:opacity-100 pointer-cursor"
-            >
-              Support
-            </a>
-          </div>
-          <div>
-            <a href=""></a>
-                
-          </div>
+      <div className="flex items-center pl-46 py-6 shadow dark:bg-black transition-all duration-1000">
+        <div className="flex-1">
+          <img src={logo} alt="logo" className="w-32.25" />
+        </div>
+        <div id="nav-tags" className="flex flex-1 justify-center space-x-14 dark:text-white text-gray-800">
+          <a
+            href=""
+            className=" hover:text-blue-500 pointer-cursor"
+          >
+            Signup
+          </a>
+          <a
+            href=""
+            className=" hover:text-blue-500 pointer-cursor"
+          >
+            About
+          </a>
+          <a
+            href=""
+            className=" hover:text-blue-500 pointer-cursor"
+          >
+            Products
+          </a>
+          <a
+            href=""
+            className=" hover:text-blue-500 pointer-cursor"
+          >
+            Pricing
+          </a>
+          <a
+            href=""
+            className=" hover:text-blue-500 pointer-cursor"
+          >
+            Support
+          </a>
+        
+          <Menu/>
+        
+        </div>
+        <div className="mx-16 rounded-full p-2 cursor-pointer dark:text-white dark:hover:bg-white/20 ">
+          {isDark?<Moon strokeWidth={2.5} />:<Sun strokeWidth={2.5} />}
         </div>
       </div>
     </>
