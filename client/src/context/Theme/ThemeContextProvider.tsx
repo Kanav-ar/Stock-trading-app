448f3a6767a10794972fcd325f53bff9c37802a6
Kanav-ar <kanava879@gmail.com>
2026-07-11 18:21:18 +0530

    implement theme initialization and add new page components for Home, About, Pricing, Products, Signup, and Support

--- client/src/context/Theme/ThemeContextProvider.tsx
+++ client/src/context/Theme/ThemeContextProvider.tsx
@@ -1,12 +1,30 @@
-import { useState, type ReactNode } from "react";
+import { useState,useEffect, type ReactNode } from "react";
 import { ThemeContext } from "./themeContext";
+
+const getInitialTheme = (): "light" | "dark" => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "light" || savedTheme === "dark") {
+    return savedTheme;
+  }
+
+  return window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+};
 
 export default function ThemeContextProvider({
   children,
 }: {
   children: ReactNode;
 }) {
-  const [theme, setTheme] = useState<"light" | "dark">("dark");
+  const [theme, setTheme] = useState<"light" | "dark">(getInitialTheme);
+   useEffect(() => {
+    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.add(theme);
+
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   function toggleTheme() {
     document.documentElement.classList.toggle("dark")
     setTheme((prev) => (prev === "light" ? "dark" : "light"));
