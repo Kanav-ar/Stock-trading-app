12fcae28d4d6d2823baa7d09b62cbd17d038539c
Kanav-ar <kanava879@gmail.com>
2026-07-20 01:16:11 +0530

    Add responsiveness to education section

--- client/src/components/common/OpenAccount.tsx
+++ client/src/components/common/OpenAccount.tsx
@@ -5,7 +5,7 @@ export default function OpenAccount() {
     <div className="flex flex-col items-center gap-12 ">
       <div className="flex flex-col gap-8 items-center">
         <h1 className="text-3xl font-semibold">Open a Zerodha Account</h1>
-        <p>
+        <p className="text-center">
           Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
           F&O trades.
         </p>
--- client/src/components/home/Education/Education.tsx
+++ client/src/components/home/Education/Education.tsx
@@ -4,16 +4,21 @@ import EduInfo from "./EduInfo";
 export default function Education() {
   return (
     <>
-      <div className="flex justify-around items-center">
+      <div className="flex flex-col gap-16 md:gap-7 md:flex-row md:justify-around items-center">
+        <div className="flex block md:hidden justify-start w-full">
+          <h1 className="text-3xl font-semibold">
+            Free and open market education
+          </h1>
+        </div>
         <div>
           <img src={educationVarsity} alt="Varsity image" />
         </div>
         <div className="flex flex-col gap-12">
-          <h1 className="text-3xl font-semibold">
+          <h1 className="text-3xl hidden md:block font-semibold">
             Free and open market education
           </h1>
 
-          <div className="flex flex-col max-w-lg gap-8">
+          <div className="flex flex-col md:max-w-lg gap-8">
             <EduInfo
               description={
                 "Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading."
